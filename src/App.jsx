a80b39eeeff6f9410cd0638e08b5b05be8d0adcd
Elshater6498 <elshaterm247@gmail.com>
2024-06-22 19:14:11 +0300

    adding elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+import { useRef, useState } from "react";
+
 function App() {
+  const [todos, setTodos] = useState(["text"]);
+  const inputRef = useRef();
+  const handelAddToDo = () => {
+    const text = inputRef.current.value;
+    setTodos([...todos, text]);
+    console.log(text);
+  };
   return (
-    <>
-      <h1 className="bg-red-400">Vite + React</h1>
-    </>
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="bg-red-400">to do list</h1>
+      <ul className="">
+        {todos.map((item) => {
+          return(<li className="">{item}</li>);
+        })}
+      </ul>
+      <div>
+        <input ref={inputRef} type="text" className="border" />
+        <button onClick={handelAddToDo} className="border">
+          add
+        </button>
+      </div>
+    </div>
   );
 }
 
